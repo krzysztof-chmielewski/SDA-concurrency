ddeac7646b382b54329e04ca4901bb2b0e4ad593
Krzysztof Chmielewski <chmielewski.k.s@gmail.com>
2017-11-28 06:18:53 +0100

    Task 06: increased n value in a test

--- src/test/java/com/kchmielewski/sda/concurrency/task06/IncrementMeTest.java
+++ src/test/java/com/kchmielewski/sda/concurrency/task06/IncrementMeTest.java
@@ -7,6 +7,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class IncrementMeTest {
     @Test
     public void thisIsDefinitelyWrongAssumption() throws Exception {
-        assertThat(new IncrementMe().incrementMeALot(1000)).isEqualTo(1000);
+        assertThat(new IncrementMe().incrementMeALot(100000)).isEqualTo(100000);
     }
 }
