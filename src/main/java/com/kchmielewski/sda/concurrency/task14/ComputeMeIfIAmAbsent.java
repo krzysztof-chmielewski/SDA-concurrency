48b21c279730a765847d66fb606682a5074fc7e3
Krzysztof Chmielewski <chmielewski.k.s@gmail.com>
2017-11-28 22:56:18 +0100

    Task 14

---
+++ src/main/java/com/kchmielewski/sda/concurrency/task14/ComputeMeIfIAmAbsent.java
@@ -0,0 +1,23 @@
+package com.kchmielewski.sda.concurrency.task14;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class ComputeMeIfIAmAbsent {
+    private final Map<Integer, List<Integer>> cache = new HashMap<>();
+
+    public List<Integer> createSquaresList(int n) {
+        return cache.computeIfAbsent(n, (list) -> {
+            List<Integer> result = new CopyOnWriteArrayList<>();
+            List<Integer> numbers = IntStream.range(0, n).boxed().collect(Collectors.toList());
+
+            numbers.forEach(i -> new Thread(() -> result.add(i * i)).start());
+
+            return result;
+        });
+    }
+}
