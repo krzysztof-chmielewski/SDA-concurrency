1a72691a302881209406b9c059534876ba21c5d6
Krzysztof Chmielewski <chmielewski.k.s@gmail.com>
2017-11-29 06:29:56 +0100

    Task 17

---
+++ src/main/java/com/kchmielewski/sda/concurrency/task17/EvenMoreFancyOneExecutor.java
@@ -0,0 +1,13 @@
+package com.kchmielewski.sda.concurrency.task17;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.IntStream;
+
+public class EvenMoreFancyOneExecutor {
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+
+        IntStream.rangeClosed(1, 10).boxed().forEach(i -> executorService.submit(() -> System.out.println(i)));
+    }
+}
