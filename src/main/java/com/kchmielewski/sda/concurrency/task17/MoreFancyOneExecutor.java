a0ad3299cedf014c04e7643cdf925bdcb8b7a166
Krzysztof Chmielewski <chmielewski.k.s@gmail.com>
2017-11-30 05:50:48 +0100

    Task17: more meaningful example.

--- src/main/java/com/kchmielewski/sda/concurrency/task17/MoreFancyOneExecutor.java
+++ src/main/java/com/kchmielewski/sda/concurrency/task17/MoreFancyOneExecutor.java
@@ -6,9 +6,17 @@ import java.util.stream.IntStream;
 
 public class MoreFancyOneExecutor {
     public static void main(String[] args) {
-        ExecutorService executorService = Executors.newFixedThreadPool(8);
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
 
-        IntStream.rangeClosed(1, 10).boxed().forEach(i -> executorService.submit(() -> System.out.println(i)));
+        IntStream.rangeClosed(1, 10).boxed().forEach(i -> executorService.submit(() -> {
+            try {
+                System.out.println("Sleeping for one second...");
+                Thread.sleep(1000);
+                System.out.println("And done: " + i);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }));
 
         executorService.shutdown();
     }
